ff786ac2f7a39c08bb0c734468bc31ac43ef9195
shafiqueabbasi <shafiqanwarabbasi@gmail.com>
2023-04-27 18:56:30 +0500

    react-hook-learning

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import ClassCounter from './ClassCounter';
+import HookCounter from './HookCounter';
+import HookCounterTwo from './HookCounterTwo';
+import HookCounter3 from './HookCounter3';
+import HookCounterFour from './HookCounterFour'
+
+
+
+export {
+    ClassCounter,
+    HookCounter,
+    HookCounterTwo,
+    HookCounter3,
+    HookCounterFour,
+}
